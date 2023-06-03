6eb2bf9b9626f49561c96e907618030477b21fc0
wholesome-ghoul <99685814+wholesome-ghoul@users.noreply.github.com>
2023-06-03 23:47:10 +0400

    feat: initial input component (#10)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import Button from "./packages/button";
+import { Input } from "./packages/all";
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <Button onClick={() => {}}>OI</Button>
+    <Input>OI</Input>
   </React.StrictMode>
 );
