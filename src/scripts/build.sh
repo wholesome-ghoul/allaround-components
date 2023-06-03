ec18c123419e45b7e1bb8452c1085e136c2e263e
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-04 02:11:58 +0400

    fix: add declaration file

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -5,6 +5,5 @@
 OUT=dist/packages/*/src
 
 npx tsc --build --verbose &&
-  cp src/*.d.ts $OUT &&
   yarn node-sass src/ -o $OUT --output-style compressed &&
   grep -rlZ '\.scss' ./$OUT | xargs -0 -r sed -i 's/\.scss/\.css/g'
