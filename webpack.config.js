af56bd8608493b833f47b472a6def140b8d3f041
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-03 21:02:21 +0400

    chore: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const aaConfigsWebpack = require("@allaround/configs-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const isDev = process.env.STAGING_ENV === "dev";
+
+module.exports = () => {
+  const rules = [
+    {
+      test: /\.tsx?$/,
+      exclude: /(node_modules|dist)/,
+      use: "ts-loader",
+    },
+    {
+      test: /\.s?css$/,
+      exclude: /(node_modules|dist)/,
+      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+    },
+  ];
+
+  const plugins = [
+    isDev && new ReactRefreshWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "./index.html",
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      // filename: "static/css/[name].[contenthash:8].css",
+      // chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+    }),
+    new ESLintPlugin(),
+  ].filter(Boolean);
+
+  const devServer = {
+    historyApiFallback: true,
+  };
+
+  const rest = {
+    // experiments: {
+    //   asyncWebAssembly: true,
+    // },
+  };
+
+  const webpackConfig = aaConfigsWebpack({
+    rules,
+    rest,
+    plugins,
+    devServer,
+  });
+
+  // console.log(webpackConfig);
+
+  return webpackConfig;
+};
+
