2a73f68cdd931fdbd8f371fd34d76ca2f4891cb7
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2023-06-24 04:00:31 +0400

    chore: use-local-storage hook

--- src/packages/hooks/src/index.ts
+++ src/packages/hooks/src/index.ts
@@ -2,10 +2,12 @@ import useResizeObserver from "./use-resize-observer";
 import useEventListener from "./use-event-listener";
 import useConfirm from "./use-confirm";
 import useModal from "./use-modal";
+import useLocalStorage from "./use-local-storage";
 
 export default {
   useResizeObserver,
   useEventListener,
   useConfirm,
   useModal,
+  useLocalStorage,
 };
---
+++ src/packages/hooks/src/use-local-storage.ts
@@ -0,0 +1,27 @@
+import React from "react";
+
+const useLocalStorage = <T>(
+  key: string,
+  initialValue?: string | boolean | number | T
+) => {
+  const [state, setState] = React.useState(() => {
+    try {
+      const value = window.localStorage.getItem(key);
+      return value ? JSON.parse(value) : initialValue;
+    } catch (e) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: string | ((s: string) => string)) => {
+    try {
+      value = typeof value === "function" ? value(state) : value;
+      setState(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {}
+  };
+
+  return [state, setValue];
+};
+
+export default useLocalStorage;
